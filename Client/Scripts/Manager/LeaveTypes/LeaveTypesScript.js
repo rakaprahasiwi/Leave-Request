b92b9225100543b54f04a672548123675e609808
Raka Prahasiwi <prahasiwi11@gmail.com>
2019-06-24 12:55:57 +0700

    create and fix ui

--- Client/Scripts/Manager/LeaveTypes/LeaveTypesScript.js
+++ Client/Scripts/Manager/LeaveTypes/LeaveTypesScript.js
@@ -5,6 +5,115 @@
     })
 })
 
+function Save() {
+    var province = new Object();
+    province.name = $('#Name').val();
+    $.ajax({
+        url: "/LeaveTypess/InsertOrUpdate/",
+        data: LeaveTypes,
+        success: function (result) {
+            swal({
+                title: "Saved!",
+                text: "That data has been save!",
+                type: "success"
+            },
+            function () {
+                window.location.href = '/LeaveTypess/Index/';
+            });
+            LoadIndexLeaveTypes();
+            $('#myModal').modal('hide');
+            ClearScreen();
+        }
+    });
+};
+
+function Edit() {
+    var province = new Object();
+    province.id = $('#Id').val();
+    province.name = $('#Name').val();
+    $.ajax({
+        url: "/LeaveTypess/InsertOrUpdate/",
+        data: LeaveTypes,
+        success: function (result) {
+            swal({
+                title: "Saved!",
+                text: "That data has been save!",
+                type: "success"
+            },
+            function () {
+                window.location.href = '/LeaveTypess/Index/';
+            });
+            LoadIndexLeaveTypes();
+            $('#myModal').modal('hide');
+            ClearScreen();
+        }
+    });
+};
+
+function GetById(Id) {
+    $.ajax({
+        url: "/LeaveTypes/GetById/",
+        type: "GET",
+        dataType: "json",
+        data: { id: Id },
+        success: function (result) {
+            $('#Id').val(result.Id);
+            $('#Name').val(result.Name);
+
+            $('#myModal').modal('show');
+            $('#Update').show();
+            $('#Save').hide();
+        }
+    })
+}
+
+function Delete(Id) {
+    swal({
+        title: "Are you sure?",
+        text: "You will not be able to recover this imaginary file!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, delete it!",
+        closeOnConfirm: false
+    }, function () {
+        $.ajax({
+            url: "/LeaveTypes/Delete/",
+            data: { id: Id },
+            success: function (response) {
+                swal({
+                    title: "Deleted!",
+                    text: "That data has been soft delete!",
+                    type: "success"
+                },
+                    function () {
+                        window.location.href = '/LeaveTypes/Index/';
+                    });
+            },
+            error: function (response) {
+                swal("Oops", "We couldn't connect to the server!", "error");
+            }
+        });
+    });
+}
+
+function ClearScreen() {
+    $('#Name').val('');
+    $('#Id').val('');
+    $('#Update').hide();
+    $('#Save').show();
+}
+
+function Validate() {
+    if ($('#Name').val() == "" || $('#Name').val() == " ") {
+        swal("Oops", "Please Insert Name", "error")
+    } else if ($('#Id').val() == "") {
+        Save();
+    } else {
+        Edit();
+    }
+}
+
 function LoadIndexLeaveTypes() {
     $.ajax({
         type: "GET",
@@ -19,8 +128,8 @@ function LoadIndexLeaveTypes() {
                 html += '<td>' + i + '</td>';
                 html += '<td>' + val.Name + '</td>';
                 html += '<td>' + val.Value + '</td>';
-                html += '<td>' + '<a href = "#" class="fa da-pencil" onclick= return GetById(' + val.Id + ')">Edit</a>';
-                html += ' | <a href="#" class="fa fa-trash" onclick=return Delete(' + val.Id + ')">Delete</a></td>';
+                html += '<td>' + '<a href = "#" class="fa fa-pencil" onclick= "return GetById(' + val.Id + ')">Edit</a>';
+                html += ' | <a href="#" class="fa fa-trash" onclick="return Delete(' + val.Id + ')">Delete</a></td>';
                 html += '</tr>';
                 i++;
             });
--- Client/Scripts/Manager/StatusTypeParameter/StatusTypeParameterScript.js
+++ Client/Scripts/Manager/StatusTypeParameter/StatusTypeParameterScript.js
@@ -5,6 +5,115 @@
     })
 })
 
+function Save() {
+    var status = new Object();
+    status.name = $('#Name').val();
+    $.ajax({
+        url: "/LeaveTypess/InsertOrUpdate/",
+        data: StatusTypeParameter,
+        success: function (result) {
+            swal({
+                title: "Saved!",
+                text: "That data has been save!",
+                type: "success"
+            },
+            function () {
+                window.location.href = '/StatusTypeParameters/Index/';
+            });
+            LoadIndexLeaveTypes();
+            $('#myModal').modal('hide');
+            ClearScreen();
+        }
+    });
+};
+
+function Edit() {
+    var province = new Object();
+    province.id = $('#Id').val();
+    province.name = $('#Name').val();
+    $.ajax({
+        url: "/LeaveTypess/InsertOrUpdate/",
+        data: LeaveTypes,
+        success: function (result) {
+            swal({
+                title: "Saved!",
+                text: "That data has been save!",
+                type: "success"
+            },
+            function () {
+                window.location.href = '/LeaveTypess/Index/';
+            });
+            LoadIndexLeaveTypes();
+            $('#myModal').modal('hide');
+            ClearScreen();
+        }
+    });
+};
+
+function GetById(Id) {
+    $.ajax({
+        url: "/LeaveTypes/GetById/",
+        type: "GET",
+        dataType: "json",
+        data: { id: Id },
+        success: function (result) {
+            $('#Id').val(result.Id);
+            $('#Name').val(result.Name);
+
+            $('#myModal').modal('show');
+            $('#Update').show();
+            $('#Save').hide();
+        }
+    })
+}
+
+function Delete(Id) {
+    swal({
+        title: "Are you sure?",
+        text: "You will not be able to recover this imaginary file!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, delete it!",
+        closeOnConfirm: false
+    }, function () {
+        $.ajax({
+            url: "/StatusTypeParameters/Delete/",
+            data: { id: Id },
+            success: function (response) {
+                swal({
+                    title: "Deleted!",
+                    text: "That data has been soft delete!",
+                    type: "success"
+                },
+                    function () {
+                        window.location.href = '/LeaveTypes/Index/';
+                    });
+            },
+            error: function (response) {
+                swal("Oops", "We couldn't connect to the server!", "error");
+            }
+        });
+    });
+}
+
+function ClearScreen() {
+    $('#Name').val('');
+    $('#Id').val('');
+    $('#Update').hide();
+    $('#Save').show();
+}
+
+function Validate() {
+    if ($('#Name').val() == "" || $('#Name').val() == " ") {
+        swal("Oops", "Please Insert Name", "error")
+    } else if ($('#Id').val() == "") {
+        Save();
+    } else {
+        Edit();
+    }
+}
+
 function LoadIndexStatusTypeParameter() {
     $.ajax({
         type: "GET",
@@ -18,8 +127,8 @@ function LoadIndexStatusTypeParameter() {
                 html += '<tr>';
                 html += '<td>' + i + '</td>';
                 html += '<td>' + val.Name + '</td>';
-                html += '<td>' + '<a href = "#" class="fa da-pencil" onclick= return GetById(' + val.Id + ')">Edit</a>';
-                html += ' | <a href="#" class="fa fa-trash" onclick=return Delete(' + val.Id + ')">Delete</a></td>';
+                html += '<td>' + '<a href = "#" class="fa fa-pencil" onclick= "return GetById(' + val.Id + ')">Edit</a>';
+                html += ' | <a href="#" class="fa fa-trash" onclick="return Delete(' + val.Id + ')">Delete</a></td>';
                 html += '</tr>';
                 i++;
             });
